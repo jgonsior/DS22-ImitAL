291f1341d8ba829c95f48cac7f9a4751c2605ed1
Julius Gonsior <julius@gaenseri.ch>
2020-06-30 14:58:47 +0200

    maybe fixed weakClust

--- active_learning/weak_supervision/weakClust.py
+++ active_learning/weak_supervision/weakClust.py
@@ -14,44 +14,40 @@ class WeakClust(BaseWeakSupervision):
         cluster_found = False
 
         # check if the most prominent label for one cluster can be propagated over to the rest of it's cluster
-        for cluster_id in self.data_storage.get_df("train")["cluster"].unique():
-            cluster_indices = self.data_storage.get_df(
-                "train", clusters=[cluster_id]
-            ).index
-            if (
-                cluster_id
-                not in self.data_storage.X_train_unlabeled_cluster_indices.keys()
-            ):
-                continue
-            if (
-                len(cluster_indices)
-                / len(self.data_storage.X_train_unlabeled_cluster_indices[cluster_id])
-                > self.MINIMUM_CLUSTER_UNITY_SIZE
-            ):
-                frequencies = collections.Counter(
-                    self.data_storage.Y_train_labeled.loc[cluster_indices][0].tolist()
-                )
+        for cluster_id in self.data_storage.get_df("train", unlabeled=True)[
+            "cluster"
+        ].unique():
+            cluster_ys = self.data_storage.get_df("train", clusters=[cluster_id])[
+                "label"
+            ]
+            cluster_indices = cluster_ys.index
+            label_frequencies = collections.Counter(cluster_ys)
 
+            if (
+                1 - label_frequencies[-1] / len(cluster_indices)
+            ) > self.MINIMUM_CLUSTER_UNITY_SIZE:
                 if (
-                    frequencies.most_common(1)[0][1]
+                    label_frequencies.most_common(1)[0][1]
                     > len(cluster_indices) * self.MINIMUM_RATIO_LABELED_UNLABELED
                 ):
-                    certain_indices = self.data_storage.X_train_unlabeled_cluster_indices[
-                        cluster_id
-                    ]
-
-                    certain_X = self.data_storage.X_train_unlabeled.loc[certain_indices]
-                    recommended_labels = [
-                        frequencies.most_common(1)[0][0] for _ in certain_indices
-                    ]
-                    recommended_labels = pd.DataFrame(
-                        recommended_labels, index=certain_X.index
+                    # contains only unlabeled samples from the cluster
+                    certain_X = (
+                        self.data_storage.get_df()
+                        .loc[cluster_indices]
+                        .loc[self.data_storage.get_df()["label"] == -1]
                     )
+                    print(self.data_storage.get_df().loc[cluster_indices])
+                    print(certain_X)
+                    certain_indices = certain_X.index
+                    recommended_labels = [
+                        label_frequencies.most_common(1)[0][0] for _ in certain_indices
+                    ]
+                    #  recommended_labels = pd.DataFrame(
+                    #      recommended_labels, index=certain_X.index
+                    #  )
                     #  log_it("Cluster ", cluster_id, certain_indices)
                     cluster_found = True
                     break
 
         # delete this cluster from the list of possible cluster for the next round
-        if cluster_found:
-            self.data_storage.X_train_labeled_cluster_indices.pop(cluster_id)
         return certain_X, recommended_labels, certain_indices, "C"
