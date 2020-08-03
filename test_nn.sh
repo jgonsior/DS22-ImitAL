859488ea3017447244fc763e8b7dfecc4e2bd188
Julius Gonsior <julius@gaenseri.ch>
2020-08-03 18:42:56 +0200

    fixed test_nn

--- test_nn.sh
+++ test_nn.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 RANDOM=42
-python single_al_cycle.py --DATASETS_PATH ../datasets --OUTPUT_CSV $1 --DATA_PATH $2 --SAMPLING $3 --CLUSTER dummy --NR_QUERIES_PER_ITERATION 5 --DATASET_NAME synthetic --START_SET_SIZE 1 --USER_QUERY_BUDGET_LIMIT 50 --RANDOM_SEED $RANDOM --N_JOBS 1 --AMOUNT_OF_PEAKED_OBJECTS 20 --MAX_AMOUNT_OF_WS_PEAKS 0& 
 
-1. fix das skript oben für den datensatz mit 40 future peaks, und für uncertainty unten
-2. das hier auf taurus als array starten -> hyper_params.csv dateien für alle 5 uncertainty strategien erstellen, und für alle neuronalen netze die ich aktuell habe
-
-# for i in $(seq 1 4);
-# do 
-#     for j in $(seq 1 7);
-#     do
-#         python single_al_cycle.py --DATASETS_PATH ../datasets --OUTPUT_CSV $1 --DATA_PATH $2 --SAMPLING $3 --CLUSTER dummy --NR_QUERIES_PER_ITERATION 5 --DATASET_NAME synthetic --START_SET_SIZE 1 --USER_QUERY_BUDGET_LIMIT 50 --RANDOM_SEED $RANDOM --N_JOBS 1 --AMOUNT_OF_PEAKED_OBJECTS 20 --MAX_AMOUNT_OF_WS_PEAKS 0& 
-#     done
-#         python single_al_cycle.py --DATASETS_PATH ../datasets --OUTPUT_CSV $1 --DATA_PATH $2 --SAMPLING $3 --CLUSTER dummy --NR_QUERIES_PER_ITERATION 5 --DATASET_NAME synthetic --START_SET_SIZE 1 --USER_QUERY_BUDGET_LIMIT 50 --RANDOM_SEED $RANDOM --N_JOBS 1 --AMOUNT_OF_PEAKED_OBJECTS 20 --MAX_AMOUNT_OF_WS_PEAKS 0 
-# done
+for i in $(seq 1 2);
+do 
+    for j in $(seq 1 7);
+    do
+        # echo $(bash -c 'RANDOM='$((42+$i+$j*100))'; echo "$RANDOM"')&
+        python single_al_cycle.py --NN_BINARY $1 --OUTPUT_DIRECTORY NN_BINARY --SAMPLING $2 --CLUSTER dummy --NR_QUERIES_PER_ITERATION 5 --DATASET_NAME synthetic --START_SET_SIZE 1 --USER_QUERY_BUDGET_LIMIT 50 --RANDOM_SEED $(bash -c 'RANDOM='$((42+$i+$j*100))'; echo "$RANDOM"') --N_JOBS 1&
+    done
+        # echo $(bash -c 'RANDOM='$((42+$i+$j*100+100))'; echo "$RANDOM"')
+        python single_al_cycle.py --NN_BINARY $1 --OUTPUT_DIRECTORY NN_BINARY --SAMPLING $2 --CLUSTER dummy --NR_QUERIES_PER_ITERATION 5 --DATASET_NAME synthetic --START_SET_SIZE 1 --USER_QUERY_BUDGET_LIMIT 50 --RANDOM_SEED $(bash -c 'RANDOM='$((42+$i+$j*100+100))'; echo "$RANDOM"') --N_JOBS 1
+done
