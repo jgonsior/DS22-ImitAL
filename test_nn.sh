d48aca35f3f4ecdfaca6412df8fd43dd392f74ab
Julius Gonsior <julius@gaenseri.ch>
2020-08-03 18:48:35 +0200

    fixerich

--- test_nn.sh
+++ test_nn.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-RANDOM=42
-
 for i in $(seq 1 2);
 do 
     for j in $(seq 1 7);
     do
         # echo $(bash -c 'RANDOM='$((42+$i+$j*100))'; echo "$RANDOM"')&
-        python single_al_cycle.py --NN_BINARY $1 --OUTPUT_DIRECTORY NN_BINARY --SAMPLING $2 --CLUSTER dummy --NR_QUERIES_PER_ITERATION 5 --DATASET_NAME synthetic --START_SET_SIZE 1 --USER_QUERY_BUDGET_LIMIT 50 --RANDOM_SEED $(bash -c 'RANDOM='$((42+$i+$j*100))'; echo "$RANDOM"') --N_JOBS 1&
+        python single_al_cycle.py --NN_BINARY $1 --OUTPUT_DIRECTORY NN_BINARY --SAMPLING trained_nn --CLUSTER dummy --NR_QUERIES_PER_ITERATION 5 --DATASET_NAME synthetic --START_SET_SIZE 1 --USER_QUERY_BUDGET_LIMIT 50 --RANDOM_SEED $(bash -c 'RANDOM='$((42+$i+$j*100))'; echo "$RANDOM"') --N_JOBS 1&
     done
         # echo $(bash -c 'RANDOM='$((42+$i+$j*100+100))'; echo "$RANDOM"')
-        python single_al_cycle.py --NN_BINARY $1 --OUTPUT_DIRECTORY NN_BINARY --SAMPLING $2 --CLUSTER dummy --NR_QUERIES_PER_ITERATION 5 --DATASET_NAME synthetic --START_SET_SIZE 1 --USER_QUERY_BUDGET_LIMIT 50 --RANDOM_SEED $(bash -c 'RANDOM='$((42+$i+$j*100+100))'; echo "$RANDOM"') --N_JOBS 1
+        python single_al_cycle.py --NN_BINARY $1 --OUTPUT_DIRECTORY NN_BINARY --SAMPLING trained_nn --CLUSTER dummy --NR_QUERIES_PER_ITERATION 5 --DATASET_NAME synthetic --START_SET_SIZE 1 --USER_QUERY_BUDGET_LIMIT 50 --RANDOM_SEED $(bash -c 'RANDOM='$((42+$i+$j*100+100))'; echo "$RANDOM"') --N_JOBS 1
 done
