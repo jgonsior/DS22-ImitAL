2cc8e2ae476b5f75fde72e962204dd4adead4475
Julius Gonsior <julius@gaenseri.ch>
2020-08-04 15:16:33 +0200

    remove active learning

--- active_learning/cluster_strategies/dummyClusterStrategy.py
+++
@@ -1,6 +0,0 @@
-from .baseClusterStrategy import BaseClusterStrategy
-
-
-class DummyClusterStrategy(BaseClusterStrategy):
-    def get_cluster_indices(self, **kwargs):
-        return self.data_storage.train_unlabeled_cluster_indices
