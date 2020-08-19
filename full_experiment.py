2d4132caeb3ad1a94ce9fc8795da65b15d52deec
Julius Gonsior <julius@gaenseri.ch>
2020-08-19 12:09:12 +0200

    last merge?

--- full_experiment.py
+++ full_experiment.py
@@ -5,26 +5,27 @@ import multiprocessing
 from pathlib import Path
 from joblib import Parallel, delayed
 
-#  VARIABLE_DATASET_TRAINING = True
-VARIABLE_DATASET_TRAINING = False
-VARIABLE_DATASET_EVAL = False
-comparisons = ["random", "uncertainty_max_margin"]
-NR_LEARNING_SAMPLES = 100
-NR_EVALUATIONS = 100
 PARENT_OUTPUT_DIRECTORY = "tmp/"
-OUTPUT_DIRECTORY = "tmp/" + str(NR_LEARNING_SAMPLES) + "_"
-REPRESENTATIVE_FEATURES = False
-AMOUNT_OF_FEATURES = -1
-HYPERCUBE = True
-OLD_SYNTHETIC_PARAMS = True
 
+params = {
+    "NR_EVALUATIONS": 100,
+    "VARIABLE_DATASET": True,
+    "NR_LEARNING_SAMPLES": 100,
+    "REPRESENTATIVE_FEATURES": False,
+    "AMOUNT_OF_FEATURES": -1,
+    "HYPERCUBE": True,
+    "OLD_SYNTHETIC_PARAMS": False,
+}
 
-if VARIABLE_DATASET_TRAINING:
-    VARIABLE_APPENDIX = "variable"
-else:
-    VARIABLE_APPENDIX = "fixed"
+param_string = ""
 
-OUTPUT_DIRECTORY += VARIABLE_APPENDIX
+for k, v in params.items():
+    if type(v) == bool:
+        param_string += "_" + k.lower()
+    else:
+        param_string += "_" + str(v)
+
+OUTPUT_DIRECTORY = PARENT_OUTPUT_DIRECTORY + param_string
 
 Path(OUTPUT_DIRECTORY).mkdir(parents=True, exist_ok=True)
 
@@ -36,12 +37,10 @@ print("#" * 80)
 print("\n")
 
 
-#  @todos: VARIABLE_DATASET_TRAINING wird parameter von imit_training und von eval (2 verschiedene parameter, der für eval wird in comparisons mit aufgenommen)
-# test mit neuer Codebasis
-
 if (
     not Path(OUTPUT_DIRECTORY + "/states.csv").is_file()
-    or sum(1 for l in open(OUTPUT_DIRECTORY + "/states.csv")) < NR_LEARNING_SAMPLES
+    or sum(1 for l in open(OUTPUT_DIRECTORY + "/states.csv"))
+    < params["NR_LEARNING_SAMPLES"]
 ):
 
     def create_dataset_sample(RANDOM_SEED):
@@ -62,24 +61,24 @@ if (
             + " --MAX_AMOUNT_OF_WS_PEAKS 0 "
             + " --AMOUNT_OF_LEARN_ITERATIONS 1 "
             + " --AMOUNT_OF_FEATURES "
-            + str(AMOUNT_OF_FEATURES)
+            + str(params["AMOUNT_OF_FEATURES"])
         )
 
-        if VARIABLE_DATASET_TRAINING:
+        if params["VARIABLE_DATASET"]:
             cli_arguments += " --VARIABLE_INPUT_SIZE "
-        if REPRESENTATIVE_FEATURES:
+        if params["REPRESENTATIVE_FEATURES"]:
             cli_arguments += " --REPRESENTATIVE_FEATURES "
-        if OLD_SYNTHETIC_PARAMS:
+        if params["OLD_SYNTHETIC_PARAMS"]:
             cli_arguments += " --OLD_SYNTHETIC_PARAMS "
-        if HYPERCUBE:
+        if params["HYPERCUBE"]:
             cli_arguments += " --HYPERCUBE "
 
         os.system(cli_arguments)
         return RANDOM_SEED
 
-    nr_parallel_processes = int(NR_LEARNING_SAMPLES / 10)
+    nr_parallel_processes = int(params["NR_LEARNING_SAMPLES"] / 10)
     if nr_parallel_processes == 0:
-        nr_parallel_processes = NR_LEARNING_SAMPLES + 1
+        nr_parallel_processes = params["NR_LEARNING_SAMPLES"] + 1
 
     with Parallel(n_jobs=multiprocessing.cpu_count()) as parallel:
         output = parallel(
@@ -118,7 +117,7 @@ print("\n")
 if (
     not Path(OUTPUT_DIRECTORY + "/trained_ann_evaluation.csv").is_file()
     or sum(1 for l in open(OUTPUT_DIRECTORY + "/trained_ann_evaluation.csv"))
-    < NR_EVALUATIONS
+    < params["NR_EVALUATIONS"]
 ):
 
     def run_evaluation(RANDOM_SEED):
@@ -131,16 +130,16 @@ if (
             + str(RANDOM_SEED)
             + " --N_JOBS 1"
             + " --AMOUNT_OF_FEATURES "
-            + str(AMOUNT_OF_FEATURES)
+            + str(params["AMOUNT_OF_FEATURES"])
         )
 
-        if VARIABLE_DATASET_TRAINING:
+        if params["VARIABLE_DATASET"]:
             cli_arguments += " --VARIABLE_INPUT_SIZE "
-        if REPRESENTATIVE_FEATURES:
+        if params["REPRESENTATIVE_FEATURES"]:
             cli_arguments += " --REPRESENTATIVE_FEATURES "
-        if OLD_SYNTHETIC_PARAMS:
+        if params["OLD_SYNTHETIC_PARAMS"]:
             cli_arguments += " --OLD_SYNTHETIC_PARAMS "
-        if HYPERCUBE:
+        if params["HYPERCUBE"]:
             cli_arguments += " --HYPERCUBE "
 
         os.system(cli_arguments)
@@ -149,7 +148,7 @@ if (
 
     with Parallel(n_jobs=multiprocessing.cpu_count(), backend="threading") as parallel:
         output = parallel(
-            delayed(run_evaluation)(k) for k in range(1, NR_EVALUATIONS + 1)
+            delayed(run_evaluation)(k) for k in range(1, params["NR_EVALUATIONS"] + 1)
         )
     print(output)
 
@@ -168,18 +167,29 @@ print("#" * 80)
 print("Creating classic evaluation data")
 print("#" * 80)
 print("\n")
+params = {
+    "VARIABLE_DATASET": True,
+    "comparisons": ["uncertainty_max_margin"],  # ["random", "uncertainty_max_margin"],
+    "NR_EVALUATIONS": 100,
+    "REPRESENTATIVE_FEATURES": False,
+    "AMOUNT_OF_FEATURES": -1,
+    "HYPERCUBE": True,
+    "OLD_SYNTHETIC_PARAMS": False,
+}
+
+CLASSIC_PREFIX = ""
+
+for k, v in params.items():
+    if type(v) == bool:
+        CLASSIC_PREFIX += "_" + k.lower()
+    else:
+        CLASSIC_PREFIX += "_" + str(v)
+
 
 # check if the other evaluation csvs already exist
-for comparison in comparisons:
+for comparison in params["comparisons"]:
     COMPARISON_PATH = (
-        PARENT_OUTPUT_DIRECTORY
-        + "classics/"
-        + comparison
-        + "_"
-        + VARIABLE_APPENDIX
-        + "_"
-        + str(NR_EVALUATIONS)
-        + ".csv"
+        PARENT_OUTPUT_DIRECTORY + "classics/" + comparison + CLASSIC_PREFIX + ".csv"
     )
 
     Path(COMPARISON_PATH).parent.mkdir(parents=True, exist_ok=True)
@@ -187,7 +197,7 @@ for comparison in comparisons:
     print(COMPARISON_PATH)
     if (
         not Path(COMPARISON_PATH).is_file()
-        or sum(1 for l in open(COMPARISON_PATH)) < NR_EVALUATIONS
+        or sum(1 for l in open(COMPARISON_PATH)) < params["NR_EVALUATIONS"]
     ):
 
         def run_classic_evaluation(RANDOM_SEED):
@@ -200,16 +210,16 @@ for comparison in comparisons:
                 + str(RANDOM_SEED)
                 + " --N_JOBS 1"
                 + " --AMOUNT_OF_FEATURES "
-                + str(AMOUNT_OF_FEATURES)
+                + str(params["AMOUNT_OF_FEATURES"])
             )
 
-            if VARIABLE_DATASET_TRAINING:
+            if params["VARIABLE_DATASET"]:
                 cli_arguments += " --VARIABLE_INPUT_SIZE "
-            if REPRESENTATIVE_FEATURES:
+            if params["REPRESENTATIVE_FEATURES"]:
                 cli_arguments += " --REPRESENTATIVE_FEATURES "
-            if OLD_SYNTHETIC_PARAMS:
+            if params["OLD_SYNTHETIC_PARAMS"]:
                 cli_arguments += " --OLD_SYNTHETIC_PARAMS "
-            if HYPERCUBE:
+            if params["HYPERCUBE"]:
                 cli_arguments += " --HYPERCUBE "
 
             os.system(cli_arguments)
@@ -220,7 +230,8 @@ for comparison in comparisons:
             n_jobs=multiprocessing.cpu_count(), backend="threading"
         ) as parallel:
             output = parallel(
-                delayed(run_classic_evaluation)(k) for k in range(1, NR_EVALUATIONS + 1)
+                delayed(run_classic_evaluation)(k)
+                for k in range(1, params["NR_EVALUATIONS"] + 1)
             )
         print(output)
     assert os.path.exists(COMPARISON_PATH)
@@ -234,38 +245,26 @@ print("#" * 80)
 print("\n")
 
 #  -> hier drinnen fehlt 1000_fixed und so :)
-comparison_path = (
-    PARENT_OUTPUT_DIRECTORY
-    + VARIABLE_APPENDIX
-    + "_"
-    + str(NR_LEARNING_SAMPLES)
-    + "_comparison_"
-    + str(NR_EVALUATIONS)
-    + "_"
-    + "_".join(comparisons)
-    + ".csv"
-)
+comparison_path = OUTPUT_DIRECTORY + "_".join(params["comparisons"]) + ".csv"
 print(comparison_path)
 
 if not Path(comparison_path).is_file():
     df = pd.read_csv(
         OUTPUT_DIRECTORY + "/trained_ann_evaluation.csv",
         index_col=None,
-        nrows=1 + NR_EVALUATIONS,
+        nrows=1 + params["NR_EVALUATIONS"],
     )
 
-    for comparison in comparisons:
+    for comparison in params["comparisons"]:
         df2 = pd.read_csv(
             PARENT_OUTPUT_DIRECTORY
             + "classics/"
             + comparison
             + "_"
-            + VARIABLE_APPENDIX
-            + "_"
-            + str(NR_EVALUATIONS)
+            + CLASSIC_PREFIX
             + ".csv",
             index_col=None,
-            nrows=1 + NR_EVALUATIONS,
+            nrows=1 + params["NR_EVALUATIONS"],
         )
         df = pd.concat([df, df2])
 
