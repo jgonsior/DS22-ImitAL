9f6c0d6e95539797c5a23716115bce690188af60
Julius Gonsior <julius@gaenseri.ch>
2020-08-20 17:30:10 +0200

    fuller not full yet

--- fuller_experiment.py
+++ fuller_experiment.py
@@ -17,21 +17,73 @@ config = standard_config(
         (["--RANDOM_SEED"], {"default": 1}),
         (["--LOG_FILE"], {"default": "log.txt"}),
         (["--OUTPUT_DIRECTORY"], {"default": "/tmp"}),
-        (["--TRAIN_VARIABLE_DATASET"], {"default": "experiment"}),
-        (["--TRAIN_NR_LEARNING_SAMPLES"], {"default": "experiment"}),
-        (["--TRAIN_REPRESENTATIVE_FEATURES"], {"default": "experiment"}),
-        (["--TRAIN_AMOUNT_OF_FEATURES"], {"default": "experiment"}),
-        (["--TRAIN_HYPERCUBE"], {"default": "experiment"}),
-        (["--TRAIN_OLD_SYNTHETIC_PARAMS"], {"default": "experiment"}),
-        (["--TEST_VARIABLE_DATASET"], {"default": "experiment"}),
-        (["--TEST_NR_LEARNING_SAMPLES"], {"default": "experiment"}),
-        (["--TEST_REPRESENTATIVE_FEATURES"], {"default": "experiment"}),
-        (["--TEST_AMOUNT_OF_FEATURES"], {"default": "experiment"}),
-        (["--TEST_HYPERCUBE"], {"default": "experiment"}),
-        (["--TEST_OLD_SYNTHETIC_PARAMS"], {"default": "experiment"}),
-        (["--TEST_COMPARISONS"], {"default": "experiment"}),
+        (["--TRAIN_VARIABLE_DATASET"], {"default": "default"}),
+        (["--TRAIN_NR_LEARNING_SAMPLES"], {"default": "default"}),
+        (["--TRAIN_REPRESENTATIVE_FEATURES"], {"default": "default"}),
+        (["--TRAIN_AMOUNT_OF_FEATURES"], {"default": "default"}),
+        (["--TRAIN_HYPERCUBE"], {"default": "default"}),
+        (["--TRAIN_OLD_SYNTHETIC_PARAMS"], {"default": "default"}),
+        (["--TEST_VARIABLE_DATASET"], {"default": "default"}),
+        (["--TEST_NR_LEARNING_SAMPLES"], {"default": "default"}),
+        (["--TEST_REPRESENTATIVE_FEATURES"], {"default": "default"}),
+        (["--TEST_AMOUNT_OF_FEATURES"], {"default": "default"}),
+        (["--TEST_HYPERCUBE"], {"default": "default"}),
+        (["--TEST_OLD_SYNTHETIC_PARAMS"], {"default": "default"}),
+        (["--TEST_COMPARISONS"], {"default": "default"}),
     ],
     standard_args=False,
 )
 
-cli_command = ""
+#  count_non_default = 2 ** (sum([1 for v in vars(config).values() if v != "default"]) - 3)
+#  print(count_non_default)
+
+
+cli_commands = {0: "python fuller_experiment.py"}
+
+
+#  liste erstellen mit [a,None], [a, b, c], [d, e, c], [f,None] und dann daraus versuchen mit itertools die permutationen zu erreichen
+arguments = []
+
+for k, v in vars(config).items():
+    if k in ["RANDOM_SEED", "LOG_FILE", "OUTPUT_DIRECTORY"]:
+        cli_commands[0] += " --" + k + " " + str(v)
+        continue
+
+    if v == "default":
+        continue
+
+    splitted_inputs = str(v).split(",")
+    if len(splitted_inputs) == 1:
+        arguments.append((k, [None, splitted_inputs[0]]))
+    elif len(splitted_inputs) > 1:
+        arguments.append((k, splitted_inputs))
+print(arguments)
+
+for k, argument in arguments:
+    offset = len(cli_commands)
+    # duplicate list
+    for i in range(0, len(cli_commands) * len(argument)):
+        print(i)
+        cli_commands[i] = cli_commands[i % len(argument)]
+    print("commands")
+    for cli_command in cli_commands.values():
+        print(cli_command)
+
+    for i, a in enumerate(argument):
+        if len(argument) == 1:
+            for j in range(offset, 2 * offset):
+                if a == "True":
+                    cli_commands[j] += " --" + k
+                else:
+                    cli_commands[j] += " --" + k + " " + str(a)
+        else:
+            for j in range(
+                offset * i, offset * i + int(len(cli_commands) / len(argument))
+            ):
+                cli_commands[j] += " --" + k + " " + str(a)
+
+
+print()
+print()
+for cli_command in cli_commands.values():
+    print(cli_command)
