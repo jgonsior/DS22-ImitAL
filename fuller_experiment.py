f8a177ad3853176a9420fbea3bd61ad79830bf83
Julius Gonsior <julius@gaenseri.ch>
2020-08-21 11:57:29 +0200

    Fixed :)

--- fuller_experiment.py
+++ fuller_experiment.py
@@ -34,14 +34,8 @@ config = standard_config(
     standard_args=False,
 )
 
-#  count_non_default = 2 ** (sum([1 for v in vars(config).values() if v != "default"]) - 3)
-#  print(count_non_default)
-
-
 cli_commands = {0: "python fuller_experiment.py"}
 
-
-#  liste erstellen mit [a,None], [a, b, c], [d, e, c], [f,None] und dann daraus versuchen mit itertools die permutationen zu erreichen
 arguments = []
 
 for k, v in vars(config).items():
@@ -57,33 +51,20 @@ for k, v in vars(config).items():
         arguments.append((k, [None, splitted_inputs[0]]))
     elif len(splitted_inputs) > 1:
         arguments.append((k, splitted_inputs))
-print(arguments)
 
 for k, argument in arguments:
     offset = len(cli_commands)
+
     # duplicate list
     for i in range(0, len(cli_commands) * len(argument)):
-        print(i)
-        cli_commands[i] = cli_commands[i % len(argument)]
-    print("commands")
-    for cli_command in cli_commands.values():
-        print(cli_command)
+        cli_commands[i] = cli_commands[i % offset]
 
     for i, a in enumerate(argument):
-        if len(argument) == 1:
-            for j in range(offset, 2 * offset):
-                if a == "True":
-                    cli_commands[j] += " --" + k
-                else:
-                    cli_commands[j] += " --" + k + " " + str(a)
-        else:
-            for j in range(
-                offset * i, offset * i + int(len(cli_commands) / len(argument))
-            ):
-                cli_commands[j] += " --" + k + " " + str(a)
+        for j in range(offset * i, offset * i + int(len(cli_commands) / len(argument))):
+            if a == None:
+                continue
+            cli_commands[j] += " --" + k + " " + str(a)
 
 
-print()
-print()
 for cli_command in cli_commands.values():
     print(cli_command)
