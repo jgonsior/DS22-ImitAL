2ced8a1afec7bb629c071540d27d17f44dc16ee9
Julius Gonsior <julius@gaenseri.ch>
2020-09-01 18:29:17 +0200

    server

--- fullllll.sh
+++ fullllll.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # python full_experiment.py --RANDOM_SEED 1 --LOG_FILE log.txt --OUTPUT_DIRECTORY tmp/ --TRAIN_CLASSIFIER MLP --TRAIN_GENERATE_NOISE --TEST_GENERATE_NOISE --TRAIN_NR_LEARNING_SAMPLES 2000 --TEST_NR_LEARNING_SAMPLES 2000 --FINAL_PICTURE tmp/plots_2000_TRAIN_CLASSIFIER_TRAIN_GENERATE_NOISE_TEST_GENERATE_NOISE/_MLP_True_True
+python fuller_experiment.py --TRAIN_LRU_AREAS_LIMIT 1,3,5 --TEST_LRU_AREAS_LIMIT 1,3,5
+python fuller_experiment.py --TRAIN_NO_DIFF_FEATURES True --TEST_NO_DIFF_FEATURES True
 python fuller_experiment.py --TRAIN_CLASSIFIER MLP,RF,SVM --TEST_CLASSIFIER MLP,RF,SVM
 python fuller_experiment.py --TRAIN_CLASSIFIER MLP,RF,SVM --TEST_CLASSIFIER MLP,RF,SVM
 python fuller_experiment.py --TRAIN_GENERATE_NOISE True --TEST_GENERATE_NOISE True
@@ -12,5 +14,3 @@ python fuller_experiment.py --TRAIN_HYPERCUBE True
 python fuller_experiment.py --TRAIN_NEW_SYNTHETIC_PARAMS True --TEST_NEW_SYNTHETIC_PARAMS True
 python fuller_experiment.py --TRAIN_CONVEX_HULL_SAMPLING True
 python fuller_experiment.py --TRAIN_STOP_AFTER_MAXIMUM_ACCURACY_REACHED True
-python fuller_experiment.py --TRAIN_LRU_AREAS_LIMIT 1,3,5 --TEST_LRU_AREAS_LIMIT 1,3,5
-python fuller_experiment.py --TRAIN_NO_DIFF_FEATURES True --TEST_NO_DIFF_FEATURES True
