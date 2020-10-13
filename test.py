c9fc71d73a4065fa563080628c495cdb1b382e91
Julius Gonsior <julius@gaenseri.ch>
2020-10-13 11:52:29 +0200

    hm

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import glob
-import os
-
-df = pd.read_csv("../datasets/taurus_10_10/MORE_DATA/dataset_creation.csv")
-
-print(df["fit_time"].mean())
-print(df["fit_time"].median())
