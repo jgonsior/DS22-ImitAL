35d96c72caa8e90aaee6a41f3a12dc7bd130f17f
Julius Gonsior <julius@gaenseri.ch>
2020-11-09 19:48:17 +0100

    First batch draft

--- optimal_experiment.py
+++ optimal_experiment.py
@@ -33,52 +33,53 @@ LIST_OF_BATCH_SAMPLING_METHODS = [
 ]
 
 if not config.SKIP_TRAINING_DATA_GENERATION:
-    for (
-        initial_batch_sampling_method,
-        initial_batch_sampling_arg,
-    ) in LIST_OF_BATCH_SAMPLING_METHODS:
-        run_parallel_experiment(
-            "Creating dataset",
-            OUTPUT_FILE=PARENT_OUTPUT_DIRECTORY
-            + train_base_param_string
-            + "_"
-            + initial_batch_sampling_method
-            + str(initial_batch_sampling_arg)
-            + "/states.csv",
-            CLI_COMMAND="python imit_training.py",
-            CLI_ARGUMENTS={
-                "DATASETS_PATH": "../datasets",
-                "OUTPUT_DIRECTORY": PARENT_OUTPUT_DIRECTORY
+    for BATCH_MODE in ["single", "batch"]:
+        for (
+            initial_batch_sampling_method,
+            initial_batch_sampling_arg,
+        ) in LIST_OF_BATCH_SAMPLING_METHODS:
+            run_parallel_experiment(
+                "Creating dataset",
+                OUTPUT_FILE=PARENT_OUTPUT_DIRECTORY
                 + train_base_param_string
                 + "_"
                 + initial_batch_sampling_method
-                + str(initial_batch_sampling_arg),
-                "DATASET_NAME": "synthetic",
-                "SAMPLING": "trained_nn",
-                "AMOUNT_OF_PEAKED_OBJECTS": config.TRAIN_AMOUNT_OF_PEAKED_SAMPLES,
-                "MAX_AMOUNT_OF_WS_PEAKS": 0,
-                "AMOUNT_OF_LEARN_ITERATIONS": 1,
-                "AMOUNT_OF_FEATURES": config.TRAIN_AMOUNT_OF_FEATURES,
-                "VARIABLE_DATASET": config.TRAIN_VARIABLE_DATASET,
-                "NEW_SYNTHETIC_PARAMS": config.TRAIN_NEW_SYNTHETIC_PARAMS,
-                "HYPERCUBE": config.TRAIN_HYPERCUBE,
-                "STOP_AFTER_MAXIMUM_ACCURACY_REACHED": config.TRAIN_STOP_AFTER_MAXIMUM_ACCURACY_REACHED,
-                "GENERATE_NOISE": config.TRAIN_GENERATE_NOISE,
-                "STATE_DISTANCES_LAB": config.TRAIN_STATE_DISTANCES_LAB,
-                "STATE_DISTANCES_UNLAB": config.TRAIN_STATE_DISTANCES_UNLAB,
-                "STATE_PREDICTED_CLASS": config.TRAIN_STATE_PREDICTED_CLASS,
-                "STATE_ARGSECOND_PROBAS": config.TRAIN_STATE_ARGSECOND_PROBAS,
-                "STATE_ARGTHIRD_PROBAS": config.TRAIN_STATE_ARGTHIRD_PROBAS,
-                "STATE_DIFF_PROBAS": config.TRAIN_STATE_DIFF_PROBAS,
-                "INITIAL_BATCH_SAMPLING_METHOD": initial_batch_sampling_method,
-                "INITIAL_BATCH_SAMPLING_ARG": initial_batch_sampling_arg,
-                **shared_arguments,
-            },
-            PARALLEL_OFFSET=0,
-            PARALLEL_AMOUNT=config.TRAIN_NR_LEARNING_SAMPLES,
-            OUTPUT_FILE_LENGTH=config.TRAIN_NR_LEARNING_SAMPLES,
-            RESTART_IF_NOT_ENOUGH_SAMPLES=True,
-        )
+                + str(initial_batch_sampling_arg)
+                + "/states.csv",
+                CLI_COMMAND="python imit_training.py",
+                CLI_ARGUMENTS={
+                    "DATASETS_PATH": "../datasets",
+                    "OUTPUT_DIRECTORY": PARENT_OUTPUT_DIRECTORY
+                    + train_base_param_string
+                    + "_"
+                    + initial_batch_sampling_method
+                    + str(initial_batch_sampling_arg),
+                    "DATASET_NAME": "synthetic",
+                    "SAMPLING": BATCH_MODE,
+                    "AMOUNT_OF_PEAKED_OBJECTS": config.TRAIN_AMOUNT_OF_PEAKED_SAMPLES,
+                    "MAX_AMOUNT_OF_WS_PEAKS": 0,
+                    "AMOUNT_OF_LEARN_ITERATIONS": 1,
+                    "AMOUNT_OF_FEATURES": config.TRAIN_AMOUNT_OF_FEATURES,
+                    "VARIABLE_DATASET": config.TRAIN_VARIABLE_DATASET,
+                    "NEW_SYNTHETIC_PARAMS": config.TRAIN_NEW_SYNTHETIC_PARAMS,
+                    "HYPERCUBE": config.TRAIN_HYPERCUBE,
+                    "STOP_AFTER_MAXIMUM_ACCURACY_REACHED": config.TRAIN_STOP_AFTER_MAXIMUM_ACCURACY_REACHED,
+                    "GENERATE_NOISE": config.TRAIN_GENERATE_NOISE,
+                    "STATE_DISTANCES_LAB": config.TRAIN_STATE_DISTANCES_LAB,
+                    "STATE_DISTANCES_UNLAB": config.TRAIN_STATE_DISTANCES_UNLAB,
+                    "STATE_PREDICTED_CLASS": config.TRAIN_STATE_PREDICTED_CLASS,
+                    "STATE_ARGSECOND_PROBAS": config.TRAIN_STATE_ARGSECOND_PROBAS,
+                    "STATE_ARGTHIRD_PROBAS": config.TRAIN_STATE_ARGTHIRD_PROBAS,
+                    "STATE_DIFF_PROBAS": config.TRAIN_STATE_DIFF_PROBAS,
+                    "INITIAL_BATCH_SAMPLING_METHOD": initial_batch_sampling_method,
+                    "INITIAL_BATCH_SAMPLING_ARG": initial_batch_sampling_arg,
+                    **shared_arguments,
+                },
+                PARALLEL_OFFSET=0,
+                PARALLEL_AMOUNT=config.TRAIN_NR_LEARNING_SAMPLES,
+                OUTPUT_FILE_LENGTH=config.TRAIN_NR_LEARNING_SAMPLES,
+                RESTART_IF_NOT_ENOUGH_SAMPLES=True,
+            )
 
 if config.ONLY_TRAINING_DATA:
     exit(1)
