db2acf1e05ff33a0d8b1710ed986672fad95a441
Julius Gonsior <julius@gaenseri.ch>
2020-11-17 18:37:55 +0100

    new way of result plotting

--- exp_joint_distributions_init_sampling_methods.py
+++ exp_joint_distributions_init_sampling_methods.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import math
 from pprint import pprint
 
@@ -13,6 +14,8 @@ from sklearn.metrics import (
     label_ranking_average_precision_score,
     dcg_score,
 )
+
+from itertools import combinations
 
 #  df = pd.read_csv("metric_test_small.csv")
 df = pd.read_csv("metric_test_50.csv")
@@ -54,144 +57,59 @@ def _jac_score_binary(a, b):
     return count_ones / TOP_N
 
 
-metrics = [
-    jaccard_score,
-    "jaccard_score_binary",
-    dcg_score,
-    ndcg_score,
-    "ndcg_score_top_k",
-    cohen_kappa_score,
-    label_ranking_loss,
-    #  label_ranking_average_precision_score,
-]
-
-evaluation = {}
-
-for metric in metrics:
-    evaluation[metric] = {key: 0 for key in df.iloc[0:AMOUNT_OF_METRICS]["source"]}
-    evaluation[metric]["maximum"] = 0
-
-
 # make uncertainty positive, add 100 to it
 for index, row in df.iterrows():
     if index % AMOUNT_OF_METRICS == 2:
         df.loc[index, df.columns != "source"] += 100
 
 
+df["source"] = df["source"].apply(
+    lambda x: x.replace("<function _calculate_", "").split(" at ")[0]
+)
+
+
 print(df.head())
 
+cummulative_scores = defaultdict(int)
+
 for i in range(0, math.ceil((len(df) / AMOUNT_OF_METRICS))):
+    #  if i > 10:
+    #      break
+    top_ks = {}
     for j in range(i * AMOUNT_OF_METRICS, (i + 1) * AMOUNT_OF_METRICS):
         if j == len(df):
             continue
-
-        if j % AMOUNT_OF_METRICS == 0:
-            current_baseline = df.iloc[j].drop("source")
-            df.loc[j, "source"] = "maximum"
-
-        for metric in metrics:
-            metric_function = metric
-            kwargs = {}
-            if metric == jaccard_score or metric == cohen_kappa_score:
-                # top_n rankings
-                baseline = (-current_baseline).argsort(axis=0)[:TOP_N].to_numpy()
-                to_compare = (
-                    (-df.iloc[j].drop("source")).argsort(axis=0)[:TOP_N].to_numpy()
-                )
-
-                if metric == jaccard_score:
-                    metric_function = _jac_score
-                else:
-                    baseline = sorted(baseline)
-                    to_compare = sorted(to_compare)
-            elif metric == "jaccard_score_binary":
-                baseline = _binarize_targets(current_baseline, TOP_N=TOP_N)[0].tolist()
-                to_compare = _binarize_targets(df.iloc[j].drop("source"), TOP_N=TOP_N)[
-                    0
-                ].tolist()
-                metric_function = _jac_score_binary
-            elif (
-                metric == ndcg_score
-                or metric == "ndcg_score_top_k"
-                or metric == dcg_score
-            ):
-                # true values
-                baseline = np.reshape(current_baseline.to_numpy(), (1, NR_BATCHES))
-                to_compare = np.reshape(
-                    df.iloc[j].drop("source").to_numpy(), (1, NR_BATCHES)
-                )
-                if metric == "ndcg_score_top_k" or metric == dcg_score:
-                    kwargs = {"k": TOP_N}
-                    metric_function = ndcg_score
-            elif metric == label_ranking_loss:
-                # 000111000 arrays
-                baseline = _binarize_targets(current_baseline, TOP_N=TOP_N)
-                to_compare = _binarize_targets(df.iloc[j].drop("source"), TOP_N=TOP_N)
-            elif metric == label_ranking_average_precision_score:
-                baseline = _binarize_targets(current_baseline, TOP_N=TOP_N)
-                to_compare = np.reshape(
-                    df.iloc[j].drop("source").to_numpy(), (1, NR_BATCHES)
-                )
-            #  print(metric)
-            #  print(df.iloc[j]["source"])
-            #  if metric == jaccard_score:
-            #      print(sorted(baseline))
-            #      print(sorted(to_compare))
-            #  else:
-            #      print(baseline)
-            #      print(to_compare)
-            #  print(metric_function(to_compare, baseline, **kwargs))
-            #  print()
-            evaluation[metric][df.iloc[j]["source"]] += metric_function(
-                baseline, to_compare, **kwargs
-            )
-    #      print("#" * 80)
-    #      print()
-    #      print()
+        top_ks[df.iloc[j]["source"]] = (
+            (-df.iloc[j].drop("source")).argsort(axis=0)[:TOP_N].to_numpy()
+        )
+    #  pprint(top_ks)
+    for a, b in combinations(top_ks.keys(), 2):
+        cummulative_scores[str(a) + " --- " + str(b)] += _jac_score(
+            top_ks[a], top_ks[b]
+        )
+    #  pprint(cummulative_scores)
     #  exit(-1)
 
-# per metric a bar chart
-pprint(evaluation)
+pprint(cummulative_scores)
+
 
 sns.set_theme(style="whitegrid")
-df = pd.DataFrame(
-    data=None, index=None, columns=["eval_metric", "value", "sampling_method"]
-)
+df = pd.DataFrame(data=None, index=None, columns=["cummulative_scores", "value"])
 
-minimum_values = []
-maximum_values = []
-
-for sampling_method, d1 in evaluation.items():
-    for eval_metric, value in d1.items():
-        if "future" in eval_metric:
-            continue
-        df = df.append(
-            {
-                "eval_metric": eval_metric.replace("<function _calculate_", "").split(
-                    " at "
-                )[0],
-                "value": value,
-                "sampling_method": str(sampling_method)
-                .replace("<function ", "")
-                .split(" at ")[0],
-            },
-            ignore_index=True,
-        )
-    minimum_values.append(min(list(d1.values())[1:]))
-    maximum_values.append(max(list(d1.values())[1:]))
+for sampling_method, value in cummulative_scores.items():
+    df = df.append(
+        {
+            "cummulative_scores": sampling_method,
+            "value": value,
+        },
+        ignore_index=True,
+    )
 df["value"] = df["value"].astype(float)
 print(df)
 
-ax = sns.catplot(
-    y="eval_metric",
+ax = sns.barplot(
+    y="cummulative_scores",
     x="value",
-    col="sampling_method",
     data=df,
-    kind="bar",
-    legend=True,
-    sharex=False,
 )
-print(list(zip(minimum_values, maximum_values)))
-for i, subplot in enumerate(ax.axes_dict.values()):
-    subplot.set_xlim(minimum_values[i], maximum_values[i])
 plt.show()
