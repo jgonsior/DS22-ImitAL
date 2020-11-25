d76bc7793317710764fa080ea4302fced54d4812
Julius Gonsior <julius@gaenseri.ch>
2020-11-25 15:16:47 +0100

    slurm test

--- slurms/slurm_test.py
+++ slurms/slurm_test.py
@@ -1,12 +1,13 @@
 import multiprocessing
 import time
 import subprocess
-import psutil
+
+#  import psutil
 import os
 
 #  text = subprocess.run(["top", "-p", "1", "-n", "1"], stdout=subprocess.PIPE)
 #  print(text.stdout.decode("utf-8"))
 print(multiprocessing.cpu_count())
-print(psutil.cpu_count())
+#  print(psutil.cpu_count())
 print(os.cpu_count())
 print(len(os.sched_getaffinity(0)))
