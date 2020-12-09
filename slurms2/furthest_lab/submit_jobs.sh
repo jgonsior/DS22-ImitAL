4b7fc7a24ed2e7d7609976f3284beccd6bac71ac
Julius Gonsior <julius@gaenseri.ch>
2020-12-09 14:23:29 +0100

    added create classics to furthest_lab

--- slurms2/furthest_lab/submit_jobs.sh
+++ slurms2/furthest_lab/submit_jobs.sh
@@ -3,6 +3,6 @@ ann_training_data_id=$(sbatch --parsable /lustre/ssd/ws/s5968580-IL_TD2/imitatin
 train_ann_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal/slurms2/furthest_lab/train_ann.slurm)
 
 create_ann_eval_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id:$train_ann_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal//slurms2/furthest_lab/ann_eval_data.slurm)
-
-plots_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id:$create_ann_eval_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal//slurms2/furthest_lab/plots.slurm)
+classics_id=$(sbatch --parsable /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal//slurms2/furthest_lab/classics.slurm)
+plots_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id:$create_ann_eval_id:$classics_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal//slurms2/furthest_lab/plots.slurm)
 exit 0
