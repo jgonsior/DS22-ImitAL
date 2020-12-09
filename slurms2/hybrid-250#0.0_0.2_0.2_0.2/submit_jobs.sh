2bdd7a945ec048490224c6437eec9ffc13d7fc39
Julius Gonsior <julius@gaenseri.ch>
2020-12-09 11:04:10 +0100

    slurm2 huiuiui

---
+++ slurms2/hybrid-250#0.0_0.2_0.2_0.2/submit_jobs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ann_training_data_id=$(sbatch --parsable /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal/slurms2/hybrid-250#0.0_0.2_0.2_0.2/ann_training_data.slurm)
+train_ann_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal/slurms2/hybrid-250#0.0_0.2_0.2_0.2/train_ann.slurm)
+
+create_ann_eval_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id:$train_ann_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal//slurms2/hybrid-250#0.0_0.2_0.2_0.2/ann_eval_data.slurm)
+
+plots_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id:$create_ann_eval_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal//slurms2/hybrid-250#0.0_0.2_0.2_0.2/plots.slurm)
+exit 0
