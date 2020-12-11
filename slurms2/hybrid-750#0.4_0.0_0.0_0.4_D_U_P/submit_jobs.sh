b75b7131a1e33654cf257c559bfe8749472185a9
Julius Gonsior <julius@gaenseri.ch>
2020-12-11 12:14:17 +0100

    batch state tests

--- slurms2/hybrid-750#0.4_0.0_0.0_0.4_D_U_P/submit_jobs.sh
+++ slurms2/hybrid-750#0.4_0.0_0.0_0.4_D_U_P/submit_jobs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ann_training_data_id=$(sbatch --parsable /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal/slurms2/hybrid-750#0.4_0.0_0.0_0.4_D_U_P/ann_training_data.slurm)
 train_ann_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal/slurms2/hybrid-750#0.4_0.0_0.0_0.4_D_U_P/train_ann.slurm)
-hyper_search_id=$(sbatch --parsable --dependency=afterok:$train_ann_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal/slurms2/hybrid-750#0.4_0.0_0.0_0.4_D_U_P/hyper_search.slurm)
-create_ann_eval_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id:$train_ann_id:$hyper_search_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal//slurms2/hybrid-750#0.4_0.0_0.0_0.4_D_U_P/ann_eval_data.slurm)
+
+create_ann_eval_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id:$train_ann_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal//slurms2/hybrid-750#0.4_0.0_0.0_0.4_D_U_P/ann_eval_data.slurm)
 
 plots_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id:$create_ann_eval_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal//slurms2/hybrid-750#0.4_0.0_0.0_0.4_D_U_P/plots.slurm)
 exit 0
