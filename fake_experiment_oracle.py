ef4b0aed0a2040543c21caac44739a10585b631b
Julius Gonsior <julius@gaenseri.ch>
2021-01-27 15:37:58 +0100

    return metrics in oracle method

--- fake_experiment_oracle.py
+++ fake_experiment_oracle.py
@@ -2,5 +2,5 @@ from active_learning.BaseOracle import BaseOracle
 
 
 class FakeExperimentOracle(BaseOracle):
-    def get_labeled_samples(self, query_indices, data_storage):
+    def get_labeled_samples(self, query_indices, data_storage, metrics_per_al_cycle):
         return data_storage.get_true_label(query_indices)
