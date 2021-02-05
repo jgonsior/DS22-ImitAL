f38205aab5dd21e618400dcdc45e3b5d97c171a3
Julius Gonsior <julius@gaenseri.ch>
2021-02-05 20:51:38 +0100

    vscode

--- fake_experiment_oracle.py
+++
@@ -1,6 +0,0 @@
-from active_learning.BaseOracle import BaseOracle
-
-
-class FakeExperimentOracle(BaseOracle):
-    def get_labeled_samples(self, query_indices, data_storage, metrics_per_al_cycle):
-        return data_storage.get_true_label(query_indices)
