3c8debf33f78bf8ab571d92732f384263eb92ea6
Julius Gonsior <julius@gaenseri.ch>
2021-03-31 17:18:35 +0200

    removed old slurm files

--- slurms2/hybrid-250#0.4_0.0_0.0_0.4/submit_jobs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ann_training_data_id=$(sbatch --parsable /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal/slurms2/hybrid-250#0.4_0.0_0.0_0.4/ann_training_data.slurm)
-train_ann_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal/slurms2/hybrid-250#0.4_0.0_0.0_0.4/train_ann.slurm)
-
-create_ann_eval_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id:$train_ann_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal//slurms2/hybrid-250#0.4_0.0_0.0_0.4/ann_eval_data.slurm)
-
-plots_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id:$create_ann_eval_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal//slurms2/hybrid-250#0.4_0.0_0.0_0.4/plots.slurm)
-exit 0
--- slurms2/hybrid-750#0.4_0.0_0.0_0.4_D_P/submit_jobs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ann_training_data_id=$(sbatch --parsable /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal/slurms2/hybrid-750#0.4_0.0_0.0_0.4_D_P/ann_training_data.slurm)
-train_ann_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal/slurms2/hybrid-750#0.4_0.0_0.0_0.4_D_P/train_ann.slurm)
-
-create_ann_eval_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id:$train_ann_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal//slurms2/hybrid-750#0.4_0.0_0.0_0.4_D_P/ann_eval_data.slurm)
-
-plots_id=$(sbatch --parsable --dependency=afterok:$ann_training_data_id:$create_ann_eval_id /lustre/ssd/ws/s5968580-IL_TD2/imitating-weakal//slurms2/hybrid-750#0.4_0.0_0.0_0.4_D_P/plots.slurm)
-exit 0
